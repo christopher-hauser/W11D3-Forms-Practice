6ea259543abcf3a72577fe401b05528ea16ce69c
Christopher Hauser <chauser6@gmail.com>
2021-11-03 16:36:23 -0400

    working on phase 3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const csrf = require('csurf');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
+const { urlencoded } = require("express");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -25,7 +26,42 @@ app.get("/", (req, res) => {
 });
 
 app.get("/create", csurfProtection, (req, res) => {
-  res.render("create", {csrfToken: req.csrfToken()})
+  res.render("create", {title: "Create", csrfToken: req.csrfToken(), errors: [], firstName: {}, lastName: {}, email: {}, password: {}})
+})
+
+const validation = (req, res, next) => {
+  const { firstName, lastName, email, password } = req.body;
+  req.errors = [];
+
+  if (!firstName) {
+    req.errors.push("Please provide a first name.");
+  }
+  if (!lastName) {
+    req.errors.push("Please provide a last name.");
+  }
+  if (!email) {
+    req.errors.push("Please provide an email.");
+  }
+  if (!password) {
+    req.errors.push("Please provide a password.");
+  }
+
+  next();
+}
+
+app.post("/create", csurfProtection, validation, (req, res) => {
+  if (req.errors.length > 0) {
+    res.render("create", {title: "Create", csrfToken: req.csrfToken(), errors: req.errors, firstName: req.body.firstName, lastName: req.body.lastName, email: req.body.email, password: req.body.password})
+  } else {
+    users.push(
+        {
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          email: req.body.email,
+          password: req.body.password
+      }
+    )
+  }
 })
 
 
